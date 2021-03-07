3c8f618024616845de18b603d0fd8e61a3c56665
Geoffrey-Magnien <78850881+Geoffrey-Magnien@users.noreply.github.com>
2021-03-07 10:51:27 +0100

    Map starts now from Paris

--- src/main/webapp/scripts/main.js
+++ src/main/webapp/scripts/main.js
@@ -5,7 +5,7 @@
  * The map chosen is one from openstreetmap because it is open source
  */
 function initMap(){
-    var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+    var mymap = L.map('mapid').setView([48.858, 2.344], 13);
 
     L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
         attribution: 'données © <a href="//osm.org/copyright">OpenStreetMap</a>/ODbL - rendu <a href="//openstreetmap.fr">OSM France</a>',
